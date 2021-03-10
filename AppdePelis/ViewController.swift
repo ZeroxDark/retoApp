74043a9efb3148521352cf109dcaa8ecfc051982
Kevin Candia Villagomez <kacandia@vectoritcgroup.com>
2021-03-09 20:02:48 -0500

    Initial Commit

---
+++ AppdePelis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppdePelis
+//
+//  Created by vector on 9/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
